e5f025d52df2eb1ca6f0e6960036ca1f029c2f62
Tiago Rodrigues <tiago.americano.03@gmail.com>
2020-09-21 22:13:25 -0300

    Adicionei o user_id

--- src/controllers/ArquivoController.js
+++ src/controllers/ArquivoController.js
@@ -2,11 +2,9 @@ const Arquivo = require('../models/Arquivo');
 
 module.exports = {
     async store(req, res) {
-        const {key, originalname, size, mimetype, path, location= ''} = req.file
-       
-        const user = await Arquivo.create({ name: originalname, key,size, type:mimetype, url:location,user_id:'1',})
-
-        return res.json(user)
+        const {key, originalname, size, mimetype, location= ''} = req.file
+        const arquivo = await Arquivo.create({ name: originalname, key,size, type:mimetype, url:location,user_id: req.body.user_id,})
+        return res.json(arquivo)
     },
 
     async index(req, res) {
