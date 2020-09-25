c4533dfbb19c7d407106dc33066652c89168fb00
GabrielZyr <64475947+Gabrielzyr@users.noreply.github.com>
2020-09-25 19:41:10 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
     dialect: 'postgres',
-    host: 'localhost',
-    username: 'postgres',
-    password: '123',
-    database: 'educanauta',
+    host: 'ec2-54-164-134-207.compute-1.amazonaws.com',
+    username: 'nlvwfbhtwhzjwm',
+    password: 'f0bbd85b673002b9866d99604a39f2e8f88c78e27742cc89a4f9ca8fed7f2d9a',
+    database: 'ddbk0chpmpq9e6',
     define: {
         timestamps: true,
         underscored: true,
